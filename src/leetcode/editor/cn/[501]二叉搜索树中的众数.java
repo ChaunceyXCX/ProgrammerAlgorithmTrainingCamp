016b2adfda1c86bd441d4378f317647ca2a2a2d7
chauncey <chaunceyxcx@gmail.com>
2022-10-13 18:01:46 +0800

    day21

---
+++ src/leetcode/editor/cn/[501]二叉搜索树中的众数.java
@@ -0,0 +1,96 @@
+//给你一个含重复值的二叉搜索树（BST）的根节点 root ，找出并返回 BST 中的所有 众数（即，出现频率最高的元素）。 
+//
+// 如果树中有不止一个众数，可以按 任意顺序 返回。 
+//
+// 假定 BST 满足如下定义： 
+//
+// 
+// 结点左子树中所含节点的值 小于等于 当前节点的值 
+// 结点右子树中所含节点的值 大于等于 当前节点的值 
+// 左子树和右子树都是二叉搜索树 
+// 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：root = [1,null,2,2]
+//输出：[2]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = [0]
+//输出：[0]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点的数目在范围 [1, 10⁴] 内 
+// -10⁵ <= Node.val <= 10⁵ 
+// 
+//
+// 
+//
+// 进阶：你可以不使用额外的空间吗？（假设由递归产生的隐式调用栈的开销不被计算在内） 
+//
+// Related Topics 树 深度优先搜索 二叉搜索树 二叉树 👍 531 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<Integer> inOrderNods = new ArrayList<>();
+    public int[] findMode(TreeNode root) {
+        if (root == null) return null;
+        getInorders(root);
+        if (inOrderNods.size() == 1){
+            return new int[]{inOrderNods.get(0)};
+        }
+        List<Integer> list = new ArrayList<>();
+        int maxCount = 0;
+        int start = 0;
+        int end = 1;
+        while (end<inOrderNods.size()){
+            if (inOrderNods.get(start) == inOrderNods.get(end)){
+                end++;
+            }else {
+                maxCount =
+            }
+        }
+    }
+
+    public void getInorders(TreeNode root) {
+        if (root == null) return;
+        //左
+        getInorders(root.letf);
+        //中
+        inOrderNods.add(root.val);
+        //右
+        getInorders(root.right);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
