ad497f021c8571c55540840bdc7d0f9f4ee3cc23
chauncey <chaunceyxcx@gmail.com>
2022-10-24 17:50:03 +0800

    day32

--- src/leetcode/editor/cn/[122]买卖股票的最佳时机 II.java
+++ src/leetcode/editor/cn/[122]买卖股票的最佳时机 II.java
@@ -45,7 +45,14 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int maxProfit(int[] prices) {
-
+        int res = 0;
+        for (int i = 1; i < prices.length; i++) {
+            int cur = prices[i]-prices[i-1];
+            if ( cur > 0){
+                res+=cur;
+            }
+        }
+        return res;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
