e04513e723f51bf7e7bba265d320cdbca1b77d4b
chauncey <chaunceyxcx@gmail.com>
2022-11-08 16:31:43 +0800

    day43

--- src/leetcode/editor/cn/[494]目标和.java
+++ src/leetcode/editor/cn/[494]目标和.java
@@ -1,4 +1,4 @@
-//给你一个整数数组 nums 和一个整数 target 。 
+// 给你一个整数数组 nums 和一个整数 target 。
 //
 // 向数组中的每个整数前添加 '+' 或 '-' ，然后串联起所有整数，可以构造一个 表达式 ： 
 //
@@ -13,9 +13,9 @@
 // 示例 1： 
 //
 // 
-//输入：nums = [1,1,1,1,1], target = 3
-//输出：5
-//解释：一共有 5 种方法让最终目标和为 3 。
+// 输入：nums = [1,1,1,1,1], target = 3
+// 输出：5
+// 解释：一共有 5 种方法让最终目标和为 3 。
 //-1 + 1 + 1 + 1 + 1 = 3
 //+1 - 1 + 1 + 1 + 1 = 3
 //+1 + 1 - 1 + 1 + 1 = 3
@@ -26,8 +26,8 @@
 // 示例 2： 
 //
 // 
-//输入：nums = [1], target = 1
-//输出：1
+// 输入：nums = [1], target = 1
+// 输出：1
 // 
 //
 // 
@@ -41,35 +41,13 @@
 // -1000 <= target <= 1000 
 // 
 //
-// Related Topics 数组 动态规划 回溯 👍 1422 👎 0
+// Related Topics 数组 动态规划 回溯 👍 1420 👎 0
 
 
-//leetcode submit region begin(Prohibit modification and deletion)
+// leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
 
-        int sum = 0;
-        for (int num : nums) {
-            sum+=num;
-        }
-
-        if (Math.abs(target)> sum){
-            return 0;
-        }
-        if ((sum+target)%2!=0){
-            return 0;
-        }
-
-        int size = (sum+target)/2;
-        if (size<0) return 0;
-        int[] dp = new int[size+1];
-        dp[0] = 1;
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = size; j >= nums[i]; j--) {
-                dp[j] += dp[j-nums[i]];
-            }
-        }
-        return dp[size];
     }
 }
-//leetcode submit region end(Prohibit modification and deletion)
+// leetcode submit region end(Prohibit modification and deletion)
