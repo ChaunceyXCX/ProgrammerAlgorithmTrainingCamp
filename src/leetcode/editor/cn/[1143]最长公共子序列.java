9b0b328020a4db9ea413110faa9806beb428cd97
chauncey <chaunceyxcx@gmail.com>
2022-11-17 22:01:29 +0800

    day52 - 没有输出笔记后补

--- src/leetcode/editor/cn/[1143]最长公共子序列.java
+++ src/leetcode/editor/cn/[1143]最长公共子序列.java
@@ -49,7 +49,18 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-
+        int[][] dp = new int[text1.length()+1][text2.length()+1];
+        for (int i = 1; i <= text1.length(); i++) {
+            for (int j = 1; j <= text2.length(); j++) {
+                //如果连续的只能是左上角推导来的，本题可以不连续所以如果左上角不能组成时，可以看下上和左
+                if (text1.charAt(i-1) == text2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1]+1;
+                }else {
+                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[text1.length()][text2.length()];
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
